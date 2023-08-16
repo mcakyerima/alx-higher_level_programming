7ffba5d7dc1c2a8da4df813d25ee43d318470f8f
mcakyerima <mcakyerima@gmail.com>
2023-08-16 22:10:29 +0100

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    module_attributes = dir(hidden_4)
+    for attribute in module_attributes:
+        if not attribute.startswith("__"):
+            print("{:s}".format(attribute))
+
