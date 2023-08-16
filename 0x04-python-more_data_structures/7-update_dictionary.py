d9a195008ffae5b60cce42b0b012e8bee29557ec
mcakyerima <mcakyerima@gmail.com>
2023-08-17 00:55:32 +0100

    fix_dic

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if a_dictionary is not None:
+        updater = {key: value}
+        a_dictionary.update(updater)
+        return(a_dictionary)
+    else:
+        return None
