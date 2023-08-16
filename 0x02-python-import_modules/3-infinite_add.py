cc3bce97f530f16e0f8fd5dd36d86e628351c86f
mcakyerima <mcakyerima@gmail.com>
2023-08-16 21:56:46 +0100

    3-infinite_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    def calculate_sum(arguments):
+        total = 0
+        for i in range(len(arguments)):
+            total += int(arguments[i])
+        return total
+
+    args = sys.argv[1:]
+    if args:
+        total_sum = calculate_sum(args)
+        print("{:d}".format(total_sum))
+    else:
+        print("No arguments provided.")
+
