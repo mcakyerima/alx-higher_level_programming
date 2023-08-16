cd0ed69817bedaad688e9ef3d9a0c42ee283ffc8
mcakyerima <mcakyerima@gmail.com>
2023-08-16 22:06:25 +0100

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     if len(command_line_arguments) > 1:
         for i in range(1, len(command_line_arguments)):
             total_sum += int(command_line_arguments[i])
-        print("{:d}".format(total_sum))
+    print("{:d}".format(total_sum))
