408e5919c72822a38439d2e39753668193c53e65
mcakyerima <mcakyerima@gmail.com>
2023-08-28 23:44:57 +0100

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[count]), end="")
             count += 1
         print()
-    except:
+    except IndexError:
         print()
     return count
