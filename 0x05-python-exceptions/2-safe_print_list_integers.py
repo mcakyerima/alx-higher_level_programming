b43fd98101b4e54ab96f547ba70bc47454ef0446
mcakyerima <mcakyerima@gmail.com>
2023-08-28 23:38:42 +0100

    safe_print_list_int

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for item in my_list:
+            if isinstance(item, int):
+                print("{:d}".format(item), end=" ")
+                count += 1
+                if count >= x:
+                    break
+
+        print()
+        return count
+    except IndexError:
+        return count
