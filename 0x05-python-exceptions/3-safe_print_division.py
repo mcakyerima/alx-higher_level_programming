aa49b036e6c2d4db19946ed1e8482db8611e7149
mcakyerima <mcakyerima@gmail.com>
2023-08-29 00:31:27 +0100

    fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
 
+def safe_print_division(a, b):
     try:
-        for i in range(list_length):
-            try:
-                element_1 = my_list_1[i]
-                element_2 = my_list_2[i]
-                result = element_1 / element_2
-                result_list.append(result)
-            except (ZeroDivisionError, TypeError):
-                result_list.append(0)
-            except IndexError:
-                print("out of range")
-                break
-    except:
-        pass  # Catch-all exception
-
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    except Exception as e:
+        print(e)
+        result = None
     finally:
-        return result_list
-
+        if result is not None:
+            print("Inside result: {}".format(result))
+        return result
