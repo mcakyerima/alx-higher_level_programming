332b20f8769c624f2f924aed32c0ef3a0f95218e
mcakyerima <mcakyerima@gmail.com>
2023-08-29 00:19:37 +0100

    bug_fix

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,30 +1,30 @@
 #include <Python.h>
 
 /**
- * print_python_bytes_info - Prints basic info about Python byte objects.
- * @byte_obj: A PyObject byte object.
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
  */
-void print_python_bytes_info(PyObject *byte_obj)
+void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
-	PyBytesObject *bytes = (PyBytesObject *)byte_obj;
+	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	fflush(stdout);
 
 	printf("[.] bytes object info\n");
-	if (strcmp(byte_obj->ob_type->tp_name, "bytes") != 0)
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	printf("  size: %ld\n", ((PyVarObject *)byte_obj)->ob_size);
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
 	printf("  trying string: %s\n", bytes->ob_sval);
 
-	if (((PyVarObject *)byte_obj)->ob_size >= 10)
+	if (((PyVarObject *)p)->ob_size >= 10)
 		size = 10;
 	else
-		size = ((PyVarObject *)byte_obj)->ob_size + 1;
+		size = ((PyVarObject *)p)->ob_size + 1;
 
 	printf("  first %ld bytes: ", size);
 	for (i = 0; i < size; i++)
@@ -38,40 +38,40 @@ void print_python_bytes_info(PyObject *byte_obj)
 }
 
 /**
- * print_python_float_info - Prints basic info about Python float objects.
- * @float_obj: A PyObject float object.
+ * print_python_float - Prints basic info about Python float objects.
+ * @p: A PyObject float object.
  */
-void print_python_float_info(PyObject *float_obj)
+void print_python_float(PyObject *p)
 {
 	char *buffer = NULL;
 
-	PyFloatObject *float_data = (PyFloatObject *)float_obj;
+	PyFloatObject *float_obj = (PyFloatObject *)p;
 
 	fflush(stdout);
 
 	printf("[.] float object info\n");
-	if (strcmp(float_obj->ob_type->tp_name, "float") != 0)
+	if (strcmp(p->ob_type->tp_name, "float") != 0)
 	{
 		printf("  [ERROR] Invalid Float Object\n");
 		return;
 	}
 
-	buffer = PyOS_double_to_string(float_data->ob_fval, 'r', 0,
+	buffer = PyOS_double_to_string(float_obj->ob_fval, 'r', 0,
 			Py_DTSF_ADD_DOT_0, NULL);
 	printf("  value: %s\n", buffer);
 	PyMem_Free(buffer);
 }
 
 /**
- * print_python_list_info - Prints basic info about Python lists.
- * @list_obj: A PyObject list object.
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
  */
-void print_python_list_info(PyObject *list_obj)
+void print_python_list(PyObject *p)
 {
 	Py_ssize_t size, alloc, i;
-	const char *element_type;
-	PyListObject *list = (PyListObject *)list_obj;
-	PyVarObject *var = (PyVarObject *)list_obj;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
 
 	size = var->ob_size;
 	alloc = list->allocated;
@@ -79,7 +79,7 @@ void print_python_list_info(PyObject *list_obj)
 	fflush(stdout);
 
 	printf("[*] Python list info\n");
-	if (strcmp(list_obj->ob_type->tp_name, "list") != 0)
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
 	{
 		printf("  [ERROR] Invalid List Object\n");
 		return;
@@ -90,11 +90,11 @@ void print_python_list_info(PyObject *list_obj)
 
 	for (i = 0; i < size; i++)
 	{
-		element_type = list->ob_item[i]->ob_type->tp_name;
-		printf("Element %ld: %s\n", i, element_type);
-		if (strcmp(element_type, "bytes") == 0)
-			print_python_bytes_info(list->ob_item[i]);
-		else if (strcmp(element_type, "float") == 0)
-			print_python_float_info(list->ob_item[i]);
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
+		else if (strcmp(type, "float") == 0)
+			print_python_float(list->ob_item[i]);
 	}
 }
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,21 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    division = None
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+
     try:
-        division = a / b
-    except (ZeroDivisionError, TypeError):
-        pass
+        for i in range(list_length):
+            try:
+                element_1 = my_list_1[i]
+                element_2 = my_list_2[i]
+                result = element_1 / element_2
+                result_list.append(result)
+            except (ZeroDivisionError, TypeError):
+                result_list.append(0)
+            except IndexError:
+                print("out of range")
+                break
+    except:
+        pass  # Catch-all exception
+
     finally:
-        print("Inside result: {}".format(quot))
-        return division
+        return result_list
+
