b383accdb17ee55738fbf2c4c481ebc3062a12f8
mcakyerima <mcakyerima@gmail.com>
2023-09-12 19:31:59 +0100

    initial

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Module lookup
+
+This module returns the list of all object's attributes and methods
+"""
+
+def lookup(obj):
+	""" returns list of all attributes and methods of a class"""
+	return dir(obj)
+
