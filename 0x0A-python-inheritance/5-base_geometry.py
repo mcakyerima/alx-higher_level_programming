c93e93efc506c0b1aa9a8580ec4de51aa41e663d
mcakyerima <mcakyerima@gmail.com>
2023-09-12 22:11:38 +0100

    baseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This module defines the BaseGeometry class.
+"""
+
+class BaseGeometry:
+    """
+    The BaseGeometry class is an empty class that serves as the base for
+    geometry-related classes.
+    """
+    pass
+
