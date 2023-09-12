558981ab184610e15715a40681ea294e8b2ae4a4
mcakyerima <mcakyerima@gmail.com>
2023-09-12 22:05:19 +0100

    fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Module 4-inherits_from.py
+
+Contains method inherits_from
+returns True if obj is instance of class that it inherits from or is subcls of
+"""
 
 def inherits_from(obj, a_class):
     """
