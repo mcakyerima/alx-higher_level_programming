9c870e48c46601d668d559b0cc1698d512d4ec26
mcakyerima <mcakyerima@gmail.com>
2023-09-12 23:28:34 +0100

    11-square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module 11-square
+
+Contains a subclass Square that inherits from Rectangle
+with instantiation of a private attribute size, validated by parent
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Inherits from Rectangle, which inherits from BaseGeometry.
+    Represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with size.
+        Args:
+            size (int): Private attribute representing the size.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Return a string representation of the square.
+        Overrides the str method to return the square description.
+        """
+        return "[Square] {}/{}".format(self.__width, self.__height)
