7cfcc3dbc94b81be95d245d1250a34075adfd7dc
mcakyerima <mcakyerima@gmail.com>
2023-09-12 22:39:51 +0100

    fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 """
-This module defines the BaseGeometry class.
+Module 7-base_geometry
+This module defines the BaseGeometry class. with public instance area and integer_validation
 """
 
 class BaseGeometry:
     """
     The BaseGeometry class is a base class for geometry-related classes.
+	Methods:
+		area(slef)
+		integer_validator(self, name, value)
     """
 
 	def area(self):
@@ -27,8 +31,7 @@ class BaseGeometry:
             TypeError: If the value is not an integer.
             ValueError: If the value is less than or equal to 0.
         """
-		if not isinstance(value, int):
-			raise TypeError(f"{name} must be an integer")
+		if type(value) is not int:
+			raise TypeError(f"{:s} must be an integer".format(name))
 		if value <= 0:
-			raise ValueError(f"{name} must be greater than 0")
-
+			raise ValueError(f"{:s} must be greater than 0".format(name))
