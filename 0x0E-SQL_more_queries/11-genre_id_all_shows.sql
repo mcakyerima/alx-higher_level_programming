91c4fa2d4bdacafa872288574a2a69a1c71267a1
mcakyerima <mcakyerima@gmail.com>
2023-10-19 03:16:40 +0100

    genre

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,15 @@
+-- To import a SQL dump, use the provided link: https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql.
+-- Create a database named 'hbtn_0d_tvshows' by running the following command:
+--   echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+-- Import the SQL dump into the 'hbtn_0d_tvshows' database using the 'curl' command.
+-- This script lists all shows contained in the 'hbtn_0d_tvshows' database.
+-- Each record in the result set displays: tv_shows.title - tv_show_genres.genre_id.
+-- The query results are sorted in ascending order based on 'tv_shows.title' and 'tv_show_genres.genre_id'.
+-- A LEFT JOIN is used to include shows without a genre, in which case 'genre_id' is displayed as NULL.
+-- This query utilizes a single SELECT statement, and the database name is expected to be passed as an argument to the MySQL command.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_shows
+LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
