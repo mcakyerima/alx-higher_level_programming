cd68f73cb8cd19c52ff1ca8469aaa2967ce05acb
mcakyerima <mcakyerima@gmail.com>
2023-10-19 02:07:00 +0100

    initial

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- script that lists all privileges of your Mysql users user_0d_1 and user_0d_2 on you mysql server
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
