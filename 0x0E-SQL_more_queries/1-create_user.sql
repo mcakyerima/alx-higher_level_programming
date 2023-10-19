8884046b7ce47ec641505c01b01382107266939f
mcakyerima <mcakyerima@gmail.com>
2023-10-19 02:39:23 +0100

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Log in to MySql SERVER with admin privileges eg (root)
 
 -- Create or update user user_0d_1 and set their password, but only if it doesn't exist
-CREATE USER IF NOT EXIST 'user_01_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user user_0d_1 on all db's and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
