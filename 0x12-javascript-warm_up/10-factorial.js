abab389b67266ab166156933f9c3763cce46cbbc
mcakyerima <mcakyerima@gmail.com>
2023-11-14 03:01:53 +0100

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const n = +process.argv[2];
+const num = parseInt(process.argv[2], 10);
 function factorial (n) {
   if (isNaN(n) || n === 0) {
     return (1);
   }
-  return (n * factorial (n - 1));
+  return (n * factorial(n - 1));
 }
-console.log(factorial(n));
+console.log(factorial(num));
