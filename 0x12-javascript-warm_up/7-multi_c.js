1c631d006e35c602d1ada587ee50ce1805d73f7b
mcakyerima <mcakyerima@gmail.com>
2023-11-14 02:25:13 +0100

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 count = parseInt(process.argv[2]);
-
 if (!isNaN(count)) {
   for (let i = 0; i < count; i++) {
     console.log('C is fun');
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-repeat = parseInt(process.argv[2]);
-
+repeat = parseInt(process.argv[2], 10);
 if (!isNaN(repeat)) {
   for (let i = 0; i < repeat; i++) {
     console.log('X'.repeat(repeat));
