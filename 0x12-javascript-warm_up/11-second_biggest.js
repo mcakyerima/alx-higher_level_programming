fbea2350138c607c2c255782ab65e97179f867b3
mcakyerima <mcakyerima@gmail.com>
2023-11-14 02:57:44 +0100

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-const inputNumbers = process.argv.slice(2);
-
+const inputNumbers = +process.argv.slice(2);
 function findSecondLargest(array) {
-  if (array.length <= 2  || array.some(isNaN)) {
-    return 0;
+  if (array.length < 2  || array.some(isNaN)) {
+    return (0);
   }
-
-  const sortedArray = array.map(Number).sort((a, b) => a - b);
-  return sortedArray[sortedArray.length -2];
+  const sortedArray = array.map(Number).sort((a, b) => a - b);  return sortedArray[sortedArray.length -2];
 }
-
 console.log(findSecondLargest(inputNumbers));
