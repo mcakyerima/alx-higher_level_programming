58ddefc26925239d31a4e777f9b17543b038a53b
mcakyerima <mcakyerima@gmail.com>
2023-11-14 02:50:48 +0100

    secondLargest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const inputNumbers = process.argv.slice(2);
+
+function findSecondLargest(array) {
+  if (array.length <= 2  || array.some(isNaN)) {
+    return 0;
+  }
+
+  const sortedArray = array.map(Number).sort((a, b) => a - b);
+  return sortedArray[sortedArray.length -2];
+}
+
+console.log(findSecondLargest(inputNumbers));
