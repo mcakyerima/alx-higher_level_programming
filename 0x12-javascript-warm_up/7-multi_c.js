6d764dbd0fef7282416f5fc133977614c3013dd8
mcakyerima <mcakyerima@gmail.com>
2023-11-14 02:02:19 +0100

    square

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ count = parseInt(process.argv[2]);
 if (!isNaN(count)) {
   for (let i = 0; i < count; i++) {
     console.log('C is fun');
-    }
+  }
 } else {
-  console.log(' Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+repeat = parseInt(process.argv[2]);
+
+if (!isNaN(repeat)) {
+  for (let i = 0; i < repeat; i++) {
+    console.log('X'.repeat(repeat));
+  }
+} else {
+    console.log('Missing size');
+}
