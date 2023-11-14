72a46a0ac40e6cce237b25a0659f261da14c6be8
mcakyerima <mcakyerima@gmail.com>
2023-11-14 03:57:42 +0100

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 const repeat = parseInt(process.argv[2], 10);
 if (!isNaN(repeat)) {
-  for (let i = 0; i < repeat; i++) {
+  for (let i = 0; i < repeat; i += 1) {
     console.log('X'.repeat(repeat));
   }
 } else {
