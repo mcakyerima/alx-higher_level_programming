be3c538a2affef4f554daf00b9149653a53067d4
mcakyerima <mcakyerima@gmail.com>
2023-11-14 03:46:12 +0100

    fix

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -5,13 +5,10 @@
  * @param {number} number - The number to be added to itself.
  * @param {Function} callback - The callback function to receive the result.
  */
-function addMeMaybe(number, callback) {
+exports.addMeMaybe = function addMeMaybe(number, callback) {
   const result = number + number;
   callback(result);
-}
-
-module.exports.addMeMaybe = addMeMaybe;
-
+};
 // Example Usage:
 // const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
 // addMeMaybe(4, function (result) {
--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function () {
-    this.value++;
-  }
 };
 
 console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
 
 myObject.incr();
 console.log(myObject);
