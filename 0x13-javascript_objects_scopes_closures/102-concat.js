9bbcd7b4640d5832582334d94e82432c3f781aa5
mcakyerima <mcakyerima@gmail.com>
2023-11-15 01:58:52 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,34 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js sourceFile1 sourceFile2 destinationFile');
-  process.exit(1);
-}
+const sourceFilePath1 = process.argv[2];
+const sourceFilePath2 = process.argv[3];
+const destinationFilePath = process.argv[4];
 
-const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
+const contentSourceFile1 = fs.readFileSync(sourceFilePath1, 'utf8');
+const contentSourceFile2 = fs.readFileSync(sourceFilePath2, 'utf8');
 
-fs.readFile(sourceFile1, 'utf8', (err1, data1) => {
-  if (err1) {
-    console.error(`Error reading ${sourceFile1}: ${err1.message}`);
-    process.exit(1);
-  }
-
-  fs.readFile(sourceFile2, 'utf8', (err2, data2) => {
-    if (err2) {
-      console.error(`Error reading ${sourceFile2}: ${err2.message}`);
-      process.exit(1);
-    }
-
-    const concatenatedData = `${data1}${data2}`;
-
-    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err3) => {
-      if (err3) {
-        console.error(`Error writing to ${destinationFile}: ${err3.message}`);
-        process.exit(1);
-      }
-
-      console.log(`${sourceFile1} and ${sourceFile2} have been concatenated to ${destinationFile}`);
-    });
-  });
-});
+fs.writeFileSync(destinationFilePath, contentSourceFile1 + contentSourceFile2);
