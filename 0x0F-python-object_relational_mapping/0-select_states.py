3f4de293f0c3ea5ad577649c2f7898ae3ff5f656
mcakyerima <mcakyerima@gmail.com>
2023-11-25 01:43:28 +0100

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
+"""
+Module: 0-select_states.py
+Connects to a MySQL server and lists all states from the database hbtn_0e_0_usa.
+"""
+
 import MySQLdb
 import sys
 
+
 def list_states(username, password, database):
+    """
+    Lists all states from the specified database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): MySQL database name.
+
+    Returns:
+        None
+    """
     # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
@@ -21,7 +38,11 @@ def list_states(username, password, database):
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
     # Check if the correct number of arguments is provided
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
@@ -32,4 +53,3 @@ if __name__ == "__main__":
 
     # Call the function to list states
     list_states(username, password, database)
-
