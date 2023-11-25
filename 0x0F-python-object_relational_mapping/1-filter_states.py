e143499ce17236159d3c0cc5650dd215a2355fb0
mcakyerima <mcakyerima@gmail.com>
2023-11-25 01:53:18 +0100

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,31 +12,32 @@ def filter_states(username, password, database):
     """
     Lists states with a name starting with 'N' from the specified database.
 
-    Args:
+	Args:
         username (str): MySQL username.
         password (str): MySQL password.
         database (str): MySQL database name.
 
     Returns:
-        None
+		None
     """
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-    cursor = db.cursor()
+	db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+	cursor = db.cursor()
 
     # Execute the SQL query to retrieve states
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+	cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     # Fetch all the rows
-    rows = cursor.fetchall()
+	rows = cursor.fetchall()
 
     # Display the results
-    for row in rows:
-        print(row)
+	for row in rows:
+		if row[1][0] == 'N':
+			print(row)
 
     # Close the database connection
-    cursor.close()
-    db.close()
+	cursor.close()
+	db.close()
 
 
 if __name__ == "__main__":
@@ -44,12 +45,12 @@ if __name__ == "__main__":
     Main entry point of the script.
     """
     # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+	if len(sys.argv) != 4:
+		print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+		sys.exit(1)
 
     # Get MySQL credentials from command-line arguments
-    username, password, database = sys.argv[1:4]
+	username, password, database = sys.argv[1:4]
 
     # Call the function to filter and list states
-    filter_states(username, password, database)
+	filter_states(username, password, database)
