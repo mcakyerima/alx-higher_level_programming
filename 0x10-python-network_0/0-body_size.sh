c4078d105351e636c6d65ae1531054721fd20797
mcakyerima <mcakyerima@gmail.com>
2023-12-01 01:37:36 +0100

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Bash Script that takes in a URL, sends a request to the URL and displays the size of the body of respose
-curl -sI $1 | grep -i Content-Length | cut -d " " -f 2
+# Use curl to send a request and display the size of the response body in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
