28b9d8136b258605186f7c6db380fdcc3d39fb9e
mcakyerima <mcakyerima@gmail.com>
2023-12-01 04:18:04 +0100

    fixed_bug

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+# Sends a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
+
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
