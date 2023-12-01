65eeeb4ad4bb4e6694771363f0150a52e9705db6
mcakyerima <mcakyerima@gmail.com>
2023-12-01 22:38:18 +0100

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,40 +1,38 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 finds the peak in a given list of unsorted integers
 """
 
-def find_peak(array):
+def find_peak(list_of_integers):
     """
     Finds a peak element in a list.
 
     Args:
-        array (list): The input list.
+        list_of_integers (list): The input list.
 
     Returns:
         int or None: The peak element if found, otherwise None.
     """
-    n = len(array)
-    if n == 0:
+
+    if len(list_of_integers) == 0:
         return None
-    if n == 1:
-        return array[0]
 
-    start, end = 0, n - 1
+    l = list_of_integers
+    beg = 0
+    end = len(l) - 1
 
-    while start < end:
-        mid = (start + end) // 2
+    if l[beg] > l[beg + 1]:
+        return l[beg]
+    if l[end] > l[end - 1]:
+        return l[end]
 
-        if array[mid] > array[mid + 1]:
-            end = mid
-        else:
-            start = mid + 1
-
-    return array[start]
-
-# Uncomment the lines below for testing with command-line arguments
-# if __name__ == "__main__":
-#     import sys
-#     args = sys.argv[1:]
-#     numbers = [int(arg) for arg in args]
-#     print(find_peak(numbers))
+    mid = (beg + end) // 2
+    if l[mid - 1] < l[mid] and l[mid + 1] < l[mid]:
+        return l[mid]
+    if l[mid] < l[mid - 1]:
+        return find_peak(l[beg : mid + 1])
+    elif l[mid] < l[mid + 1]:
+        return find_peak(l[mid : end + 1])
+    else:
+        return l[beg]
