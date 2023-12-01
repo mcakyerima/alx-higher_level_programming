e40a74627ab5479727c2f39f063c720db7b2c243
mcakyerima <mcakyerima@gmail.com>
2023-12-01 01:56:10 +0100

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Use curl to send a GET request and display the body for a 200 status code.
+# Accepts a URL, then display body of a 200 response; Usage: ./1-body.sh 5B0.0.0.0:5000/route_1 ; echo ""
 curl -sL "$1"
