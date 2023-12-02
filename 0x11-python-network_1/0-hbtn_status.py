376fe4bb8e32845a3963287e60b06f494acd9989
mcakyerima <mcakyerima@gmail.com>
2023-12-02 01:55:11 +0100

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-
-"""Script that fetches https://alx-intranet.hbtn.io/status"""
-
+"""
+Python script using urllib to fetch the status from the given URL and display the required information
+"""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status
+url = 'https://alx-intranet.hbtn.io/status'
 
-#use context manager to read content.
 with urllib.request.urlopen(url) as response:
-	content = response.read()
-	utf8_content = content.decode('utf-8')
+    content = response.read()
+    utf8_content = content.decode('utf-8')
 
-	print("Body response:")
-	print("\t- type:", type(content))
-	print("\t- content:", content)
-	print("\t- utf8 content:", utf8_content)
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", utf8_content)
