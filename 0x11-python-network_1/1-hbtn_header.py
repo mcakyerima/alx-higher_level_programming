d2a61c2e02ac2cd1a9f16bdc9b0d5a67e843eb06
mcakyerima <mcakyerima@gmail.com>
2023-12-02 02:00:50 +0100

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Python script named 1-hbtn_header.py that takes a URL as a command-line argument, sends an HTTP request, and displays the value of the X-Request-Id variable found in the response header
+"""
+
+import sys
+import urllib.request
+
+if __name__ == '__main__':
+	url = sys.argv[1]
+
+	with urllib.request.urlopen(url) as response:
+		x_request_id = response.headers.get('X-Request-Id')
+		print(x_request_id)
