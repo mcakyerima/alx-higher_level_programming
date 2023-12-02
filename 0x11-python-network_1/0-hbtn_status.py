7b724b3572cb29208e4f0768a579a42849deccdf
mcakyerima <mcakyerima@gmail.com>
2023-12-02 02:04:11 +0100

    refactor __name__=='__main__'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,14 @@ Python script using urllib to fetch the status from the given URL and display th
 """
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+	url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
+	with urllib.request.urlopen(url) as response:
+    	content = response.read()
+    	utf8_content = content.decode('utf-8')
 
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", utf8_content)
+    	print("Body response:")
+    	print("\t- type:", type(content))
+    	print("\t- content:", content)
+    	print("\t- utf8 content:", utf8_content)
