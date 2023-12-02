a3f42d23d5312f291c8a2fc05aace05fd139f981
mcakyerima <mcakyerima@gmail.com>
2023-12-02 02:27:42 +0100

    url

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script named 2-post_email.py that takes a URL and an email as command-line arguments
+sends a POST request to the specified URL with the email as a parameterand displays the body of the
+response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+	url = sys.argv[1]
+	email = sys.argv[2]
+
+	data = urllib.parse.urlencode({'email':email}).encode('utf-8')
+	req = urllib.request.Request(url, data=data, method='POST')
+
+	with urllib.request.urlopen(req) as response:
+		content = response.read().decode('utf-8')
+		print(email)
