81798626ffbca878ab67fcfb7c072e1f77b75082
mcakyerima <mcakyerima@gmail.com>
2023-12-02 03:27:20 +0100

    output_fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Accepts URL & Email as params, display response body utf-8, print error codes
+Accepts URL & Email as params, display response body utf-8, printerror codes
 usage: ./7-error_code.py http://0.0.0.0:5000/status_401
 """
 from sys import argv
@@ -8,8 +8,8 @@ import requests
 
 
 if __name__ == "__main__":
-    request = requests.get(argv[1])
-    if request.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(request.text)
+	request = requests.get(argv[1])
+	if request.status_code >= 400:
+		print("Error code: {}".format(request.status_code))
+	else:
+		print(request.text)
