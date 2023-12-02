adfb5f1bf9c00505b796e174aba2922906654154
mcakyerima <mcakyerima@gmail.com>
2023-12-02 02:48:39 +0100

    error_code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Given a URL and an Email as parameter, return a response body in utf-8 format, then print error codes.
+usage: ./3-error_code.py http://0.0.0.0:5000/status_401
+"""
+import sys
+import urllib.request
+
+if__name__ == "__main__":
+	try:
+		url = sys.argv[1]
+		with urllib.request.urlopen(url) as response:
+			content = response.read().decode('utf-8)
+			print(content)
+	except urllib.error.HTTPError as e:
+		print("Error code: {}".format(e.code))
