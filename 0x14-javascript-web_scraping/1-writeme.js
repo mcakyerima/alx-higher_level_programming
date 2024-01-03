a25cd6542c481db0f4a3a4116278dd2c1e9e13ae
mcakyerima <mcakyerima@gmail.com>
2024-01-04 00:48:24 +0100

    fixed code style

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,9 @@
 const fs = require('fs');
 let fileName = process.argv[2];
 let fileContent = process.argv[3];
-fs.writeFile(fileName, fileContent, 'utf8', (err) => {
+
+fs.writeFile(fileName, fileContent, 'utf-8', (err) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 });
