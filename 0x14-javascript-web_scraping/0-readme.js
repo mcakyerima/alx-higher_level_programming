8d327f781baf81b1079ff3300bebe45a423e78bf
mcakyerima <mcakyerima@gmail.com>
2024-01-04 00:15:15 +0100

    fixed semistandard code check

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-let filename = process.argv[2];
-fs.readFile(filename, 'utf8', (err, data) => {
+let file_name = process.argv[2];
+fs.readFile(file_name, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-     console.log(data);
+    console.log(data);
   }
 });
