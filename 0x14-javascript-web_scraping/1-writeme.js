b6f7cbf5623aa680c6a8081e16087d1eb1789f62
mcakyerima <mcakyerima@gmail.com>
2024-01-04 00:50:29 +0100

    fixed standard code style

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-let fileName = process.argv[2];
-let fileContent = process.argv[3];
+const fileName = process.argv[2];
+const fileContent = process.argv[3];
 
-fs.writeFile(fileName, fileContent, 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
+fs.writeFile(fileName, fileContent, 'utf-8',
+  (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
