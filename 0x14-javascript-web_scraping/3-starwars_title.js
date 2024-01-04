5485a27f8e0bef489646c06f284d0f8e6d90369a
mcakyerima <mcakyerima@gmail.com>
2024-01-04 01:44:06 +0100

    star wars api

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  }
+});
