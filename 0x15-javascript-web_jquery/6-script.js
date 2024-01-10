adfcba544e4662423d24acc566a4457b904ccb74
mcakyerima <mcakyerima@gmail.com>
2024-01-10 23:38:00 +0100

    changing text on click with query

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+$('document').ready(function () {
+  // Add a click event to DIV#update_header
+  $('#update_header').click(function () {
+    // Update the text of <header> to  "New Header!!!"
+    $('header').text('New Header!!!');
+  });
+});
