5c023adfc60e13cff4a42659956125a8790ca255
mcakyerima <mcakyerima@gmail.com>
2024-01-10 22:30:36 +0100

    code to change header text

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Wait for the DOM content to be fully loaded
+
+  // Select the <header> element and update its text color to red
+  var headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
