6c5c69ed4a95f023d06ff855a1e0380987715557
mcakyerima <mcakyerima@gmail.com>
2024-01-11 02:36:04 +0100

    changing color via DOM

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Allow DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the <header> element and update its text color to red
+  var headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
